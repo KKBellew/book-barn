dec70fb66870677cd1d509e3d1bf12e571e2ad34
kkbellew <kkbellew@gmail.com>
2024-02-26 11:59:12 -0600

    added mutations and queries

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,7 +1,63 @@
-`LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
+import { gql } from '@apollo/client';
 
-`ADD_USER` will execute the `addUser` mutation.
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
 
-`SAVE_BOOK` will execute the `saveBook` mutation.
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
 
-`REMOVE_BOOK` will execute the `removeBook` mutation.
+export const SAVE_BOOK = gql`
+  mutation SaveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation RemoveBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
--- Develop/client/src/utils/queries.js
+++ Develop/client/src/utils/queries.js
@@ -1,1 +1,12 @@
-`queries.js`: This will hold the query `GET_ME`, which will execute the `me` query set up using Apollo Server.
+//`queries.js`: This will hold the query `GET_ME`, which will execute the `me` query set up using Apollo Server.
+
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+  query GetMe {
+    me {
+      _id
+      username
+      email
+    }
+  }`;
