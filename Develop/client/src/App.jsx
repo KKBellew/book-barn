8e52bc61b834bea12a4d61bfdfe5c11d4dcf97ee
kkbellew <kkbellew@gmail.com>
2024-02-26 11:50:26 -0600

    apollo to app.jsx

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// Create an Apollo Client instance
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql', // Update the URI with your GraphQL server endpoint
+  cache: new InMemoryCache()
+});
+
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Navbar />
+        <Outlet />
+      </div>
+    </ApolloProvider>
   );
 }
 
